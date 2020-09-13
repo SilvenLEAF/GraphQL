5cc9d7e6b8d179a8aaf4e8f2c25b4193dad7a41e
SilvenLEAF <SilvenLEAF@gmail.com>
2020-09-13 18:59:53 +0530

    explaining how to create apis

---
+++ app.js
@@ -0,0 +1,3 @@
+
+// This line will bring ExpressJS that you installed here so that we can use it. It works just like HTML <script src="./myJavaScriptFile.js" ></script>j
+const express = require('express');
